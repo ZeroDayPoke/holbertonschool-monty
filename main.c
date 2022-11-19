cd48b508b479cc5340e88879df084e7a10c1a7a5
Chris Stamper <cstampok@gmail.com>
2022-11-19 15:05:13 -0600

    test

--- main.c
+++ main.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 	FILE *inboundFD = NULL;
 	size_t n = 0;
 	char *line, *pokeyTokey, *tokeyTwo;
-	const char delims[] = " \t";
+	const char delims[] = " \t\n";
 
 	if (argc != 2)
 	{
