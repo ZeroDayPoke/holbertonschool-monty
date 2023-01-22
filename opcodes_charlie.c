e0220d87c53a935fbca3becb6e2fb928e0eca992
Chris Stamper <cstampok@gmail.com>
2023-01-21 18:29:44 -0600

    fix error message on pchar

--- opcodes_charlie.c
+++ opcodes_charlie.c
@@ -44,7 +44,7 @@ void pchar_monty(stack_t **stack, unsigned int line_number)
 
 	if (!(*stack))
 	{
-		fprintf(stderr, "L%d: can't pchar, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	if ((*stack)->n < 0 || (*stack)->n > 127)
